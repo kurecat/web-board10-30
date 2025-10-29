7d1ef5e29dd0b7596ab15c8b6e82b9b46a418d0f
jks2024 <jks2024@gmail.com>
2025-10-29 12:39:39 +0900

    10월29일수정 12:40

---
+++ src/main/java/com/human/web_board/service/CommentService.java
@@ -0,0 +1,18 @@
+package com.human.web_board.service;
+
+import com.human.web_board.dto.CommentCreateReq;
+import com.human.web_board.dto.CommentRes;
+
+import java.util.List;
+
+public interface CommentService {
+    // 댓글 쓰기
+    Long write(CommentCreateReq req);
+
+    // 댓글 목록 조회
+    List<CommentRes> list(Long postId);
+    // 댓글 수정
+    boolean update(CommentCreateReq req, Long id);
+    // 댓글 삭제
+    boolean delete(Long id);
+}
---
+++ src/main/java/com/human/web_board/service/impl/CommentServiceImpl.java
@@ -0,0 +1,47 @@
+package com.human.web_board.service.impl;
+
+import com.human.web_board.dao.CommentDao;
+import com.human.web_board.dao.MemberDao;
+import com.human.web_board.dao.PostDao;
+import com.human.web_board.dto.CommentCreateReq;
+import com.human.web_board.dto.CommentRes;
+import com.human.web_board.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentServiceImpl implements CommentService {
+    private final CommentDao commentDao;
+    private final PostDao postDao;
+    private final MemberDao memberDao;
+    @Override
+    @Transactional
+    public Long write(CommentCreateReq req) {
+        if(memberDao.findById(req.getMemberId()) == null) {
+            throw new IllegalArgumentException("존재 하지 않은 회원 입니다.");
+        }
+        if(postDao.findById(req.getPostId()) == null) {
+            throw new IllegalArgumentException("존재하지 않는 게시글 입니다.");
+        }
+        return commentDao.save(req);
+    }
+
+    @Override
+    public List<CommentRes> list(Long postId) {
+        return commentDao.findByPostId(postId);
+    }
+
+    @Override
+    public boolean update(CommentCreateReq req, Long id) {
+        return commentDao.update(req, id);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return commentDao.delete(id);
+    }
+}
