2cd87456e6420823db4a8129c4b3d7deaa2e0299
jks2024 <jks2024@gmail.com>
2025-10-29 14:15:08 +0900

    10월29일수정 14:15

---
+++ src/main/java/com/human/web_board/controller/MemberRestController.java
@@ -0,0 +1,32 @@
+package com.human.web_board.controller;
+
+import com.human.web_board.dto.MemberRes;
+import com.human.web_board.dto.MemberSignupReq;
+import com.human.web_board.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController // 외부의 요청을 아파치 톰켓이 수신하고 해당 경로에 해당 하는 메서드 호출, 내부적 JSON 역직렬화를 해줌
+@RequiredArgsConstructor // 생성자를 통해서 의존성 주입 받기 위해 사용
+@RequestMapping("/api/members")  // http://localhost:8111/api/members
+public class MemberRestController {
+    private final MemberService memberService;
+    @PostMapping("/signup")   // Post 방식 : 정보를 Body 숨겨서 전송 하는 방식, 정보를 추가할 때 주로 사용
+    public ResponseEntity<Long> singup(@RequestBody MemberSignupReq req) {
+        return ResponseEntity.ok(memberService.signup(req));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MemberRes>> list() {
+        return ResponseEntity.ok(memberService.list());
+    }
+
+    // 이메일로 회원 조회
+    @GetMapping("/{email}")
+    public ResponseEntity<MemberRes> findByEmail(@PathVariable String email) {
+        return ResponseEntity.ok(memberService.getByEmail(email));
+    }
+}
--- src/main/java/com/human/web_board/service/impl/CommentServiceImpl.java
+++ src/main/java/com/human/web_board/service/impl/CommentServiceImpl.java
@@ -9,7 +9,6 @@ import com.human.web_board.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
 @Service
